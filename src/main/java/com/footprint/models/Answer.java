0d3ab064846de7ffab239b3643db088322d34659
NaionaraRamos <nramosmaceda@gmail.com>
2020-11-20 09:49:29 -0300

    20-11

--- src/main/java/com/footprint/models/Answer.java
+++ src/main/java/com/footprint/models/Answer.java
@@ -1,27 +1,27 @@
-package com.footprint.models;
+//package com.footprint.models;
 
-import java.util.Date;
-import java.util.List;
+//import java.util.Date;
+//import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import lombok.Data;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.OneToOne;
+//import lombok.Data;
 
-@Data
-@Entity
-public class Answer {
+//@Data
+//@Entity
+//public class Answer {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private Long id;
 	
-	@OneToOne
-	private User userId;
+//	@OneToOne
+//	private User userId;
 	
-	private Date date;
+//	private Date date;
 	
-	private List<Question> questions;
-}
+//	private List<Question> questions;
+//}
